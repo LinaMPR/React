4f4d1498532ebcbe9cbf7bf0604c722a7ceee311
Lina Paez <linapaez96@gmail.com>
2025-05-26 17:56:05 -0500

    Second exercise

---
+++ CourseOne/ExcerciseTwo/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './componentes/Card'
+import './App.css'
+
+function App() {
+
+  return (
+   <section>
+    <h1>Excersice two</h1>
+    <Card title="Card 1" description="Esta es una descripción" />
+    <Card title="Card 2" description="Esta es otra descripción"></Card>
+   </section>
+  )
+}
+
+export default App
